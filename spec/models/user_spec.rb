75301cf5a2f2d8a90a75691f853feab02134e988
umair-eritheia <umairr.murtaza@gmail.com>
2018-10-11 16:54:57 +0500

    user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # Association test
-  # ensure Todo model has a 1:m relationship with the Item model
-  it { should have_many(:items).dependent(:destroy) }
-  # Validation tests
-  # ensure columns title and created_by are present before saving
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:created_by) }
+	it { should have_many(:user_comments).dependent(:destroy) }
+	it { should have_many(:comments).through(:user_comments) }
+
+	it { should have_many(:list_users).dependent(:destroy) }
+	it { should have_many(:lists).through(:list_users) }
+
+	it { should callback(:assign_default_role).after(:create) }
+
+	it { should validate_uniqueness_of(:username) }
+	it { should validate_uniqueness_of(:email) }
+	it { should validate_presence_of(:username) }
+	it { should validate_presence_of(:email) }
 end
+
+	
