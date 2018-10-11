7dc9583bcc03f68a0667ccf23733500cc82dc142
HAMZA AZHAR <hamza@intagleo.com>
2018-10-11 17:39:02 +0500

    Removed useless models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ApplicationRecord
 	has_secure_password
 	rolify
-	has_many :user_cards, dependent: :destroy
-	has_many :cards, through: :user_cards
-	
+  
 	has_many :user_comments, dependent: :destroy
 	has_many :comments, through: :user_comments
 
